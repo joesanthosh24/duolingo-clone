8740dd8aa4b59fbb1fb6fed400f90b98b00fef6d
joesanthosh24 <joe.santhosh88@yahoo.com>
2020-08-03 06:11:45 -0400

    Styled out navbar and added content to it. Used material ui icons for dropdown icon

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
+
+import Navbar from "./components/navbar/navbar.component";
+
 import './App.css';
 
 function App() {
   return (
     <div className="app">
-      <nav>
-        
-      </nav>
+      <Navbar />
     </div>
   );
 }
--- src/components/navbar/navbar.component.jsx
+++ src/components/navbar/navbar.component.jsx
@@ -1,11 +1,30 @@
-import React from 'react';
+import React from "react";
+import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
+
+import "./navbar.styles.css";
 
 const Navbar = () => {
-    return (
-        <nav className="navbar">
-            
-        </nav>
-    );
-}
- 
+  return (
+    <nav className="navbar">
+      <img
+        width="153px"
+        height="36px"
+        className="navbar__logo"
+        src="//d35aaqx5ub95lt.cloudfront.net/images/logo-white.svg"
+        alt="duolingo logo"
+      />
+      <div className="navbar__right">
+        Site Language: <span>English</span>
+        <div className="navbar__rightLanguageDropdown">
+          <ArrowDropDownIcon />
+        </div>
+        <div className="navbar__loginSignUp">
+          <button id="signUp">Get Started</button>
+          <button id="login">Login</button>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
 export default Navbar;
